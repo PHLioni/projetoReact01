6c51a08011ccb32fdaee2a0eada4a3d92fcd37d6
Pedro Lioni <36171693+PHLioni@users.noreply.github.com>
2020-02-29 22:43:48 -0300

    AjusteExercicios

--- src/Menu.js
+++ src/Menu.js
@@ -4,22 +4,34 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
 import { Inverter, MegaSena } from './componentes/Multi'
+import Contador from './componentes/Contador'
+import Plataformas from './componentes/Plataformas'
+import ValidaProps from './componentes/ValidaProps'
 
 export default createDrawerNavigator({
+    ValidaProps:{
+        screen:() => <ValidaProps ano={18}/>
+    },
+    Plataformas:{
+        screen:() => <Plataformas/>
+    },
+    Contador: {
+        screen: () => <Contador/>
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={8} />,
         navigationOptions: { title: 'Mega Sena' }
     },
     Inverter: {
-        screen: () => <Inverter texto='Pedro' />,       
-    }, 
-    ParImpar:{
-        screen:() => <ParImpar numeros={10}/>,
+        screen: () => <Inverter texto='Pedro' />,
+    },
+    ParImpar: {
+        screen: () => <ParImpar numeros={10} />,
         navigationOptions: { title: 'Par e Impar' }
 
-    }, 
-    Simples:{
-        screen:() => <Simples texto='flexivel'/>,      
+    },
+    Simples: {
+        screen: () => <Simples texto='flexivel' />,
 
     },
-}, {drawerWidth: 300})
+}, { drawerWidth: 300 })
